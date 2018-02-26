9196e88278375c5034af060fedd1b93d17bfd702
damienpointier <36849954+damienpointier@users.noreply.github.com>
2018-02-26 15:28:19 +0100

    AnimalDAO getInstance

--- src/main/java/fr/eni/clinique/dal/dao/impl/AnimalJDBCDAOImpl.java
+++ src/main/java/fr/eni/clinique/dal/dao/impl/AnimalJDBCDAOImpl.java
@@ -1,5 +1,12 @@
 package fr.eni.clinique.dal.dao.impl;
 
+import fr.eni.clinique.dal.dao.AnimalDAO;
+
 public class AnimalJDBCDAOImpl {
+
+	public static AnimalDAO getInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
