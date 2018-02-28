15df90c15e7780587c8f26dffdc603023dc18c2d
BastienPoP <Bastien-poupelin@hotmail.fr>
2018-02-28 15:06:37 +0100

    Merge branch 'master' of https://github.com/pop2211/CliniqueVeto
    fct button

--- src/main/java/fr/eni/clinique/ihm/screen/login/InternalFrameLogin.java
+++ src/main/java/fr/eni/clinique/ihm/screen/login/InternalFrameLogin.java
@@ -51,10 +51,23 @@ public class InternalFrameLogin extends JInternalFrame {
         
         //mainPanel.add(validateButton, createGridBagConstraints(0.7, 1, 3));
         
-		buttonBar = createButtonBar();
-        mainPanel.add(buttonBar, createGridBagConstraints(1, 1, 10));
+        mainPanel.add(createButton(), createGridBagConstraints(1, 1, 10));
         
         
+	}
+	
+	private JButton createButton() {
+		validateButton = new JButton("Valider");
+	    validateButton.addActionListener(new ActionListener() {
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            if(actionListener != null) {
+	            	connectPersonnel();
+	            }
+	        }
+	    });
+	    
+	    return validateButton;
 	}
 	
     /**
