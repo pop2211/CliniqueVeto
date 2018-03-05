b2e21199f42d6e75c2d80a09ad0c56997cc62fa3
Shaldren <fouillen.dorian@orange.fr>
2018-03-05 16:34:03 +0100

    PersonnelScreen + Table
    Gné Gné Gné JPP

---
+++ src/main/java/fr/eni/clinique/ihm/model/TableModelPersonnel.java
@@ -0,0 +1,72 @@
+package fr.eni.clinique.ihm.model;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import fr.eni.clinique.bll.exception.ManagerException;
+import fr.eni.clinique.bll.manager.impl.PersonnelManagerImpl;
+import fr.eni.clinique.bo.Personnel;
+
+public class TableModelPersonnel extends AbstractTableModel{
+	
+	private final String[] entetes = { "Nom","Role","Mdp"};	
+	
+	private PersonnelManagerImpl personnelManagerImpl;
+
+	private List<Personnel> personnels;
+	
+	public TableModelPersonnel() {
+		personnelManagerImpl = PersonnelManagerImpl.getInstance();
+		try {
+			personnels = personnelManagerImpl.selectAll();
+		} catch (ManagerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Personnel> getNotes() {
+		return personnels;
+	}
+	
+
+	@Override
+	public int getColumnCount() {
+		return entetes.length;
+	}
+	
+	@Override
+	public String getColumnName(int columnIndex) {
+		return entetes[columnIndex];
+	}
+
+	@Override
+	public int getRowCount() {
+		return personnels.size();
+	}
+	
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		switch (columnIndex) {
+
+		case 0:
+			// Nom
+			return personnels.get(rowIndex).getNom();
+
+		case 1:
+			// Prenom
+			return personnels.get(rowIndex).getRole();
+
+		case 2:
+			// Annee
+			return personnels.get(rowIndex).getMdp();
+
+
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+}
